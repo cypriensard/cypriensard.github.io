a554979fc325e58cff5fe18f9dead4a5795ecfba
cypriensard <155336260+cypriensard@users.noreply.github.com>
2024-01-24 18:19:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,15 @@
-
-function change(valeur)
-{
-  var changer = document.getElementById(valeur);
-
-  if (changer.style.visibility == 'hidden')
-  {
-  changer.style.visibility = 'visible';
-  }
-  else
-  {
-  changer.style.visibility = 'hidden';
-  }
-};
-
-
 document.addEventListener("DOMContentLoaded", function() {
-    var top_position = document.getElementById("citation").offsetTop;
-    
-    // Utilisez getElementsByClassName pour sélectionner l'élément avec la classe "image_container"
-    var imageContainers = document.getElementsByClassName("image_container");
-    
-    // Vérifiez s'il y a au moins un élément avec la classe "image_container"
-    if (imageContainers.length > 0) {
-        // Accédez au premier élément de la collection et définissez le style
-        imageContainers[0].style.top = top_position + 300 + "px";
-    }
-});
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    var imageContainer = document.querySelector('.image_container');
-    var icelandElement = document.getElementById("Iceland");
+    var imageContainer = document.querySelector(".image_container");
+    var videoPartContainerElement = document.querySelector(".video-part-container");
     var whitetransElement = document.getElementById("white-trans"); 
 
-    
     function ajusterPosition() {
         // Obtenez l'altitude du bas de l'élément par rapport à son parent
         var bottomAltitude = imageContainer.offsetTop + imageContainer.offsetHeight;
 
-        // Ajustez le style de l'élément "Iceland" en utilisant l'altitude du bas de la classe "image_container"
-        icelandElement.style.top = bottomAltitude + 50 + "px";
-        whitetransElement.style.top = bottomAltitude + 50  + "px";
-
+        // Ajustez le style de l'élément "video-part-container" en utilisant l'altitude du bas de la classe "image_container"
+        videoPartContainerElement.style.top = bottomAltitude + "px";
+        whitetransElement.style.top = bottomAltitude + "px";
     }
 
     // Appelez la fonction initiale pour définir la position initiale
@@ -51,34 +19,10 @@ document.addEventListener("DOMContentLoaded", function() {
     window.addEventListener('resize', ajusterPosition);
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-
-    var icelandElement = document.getElementById("Iceland");
-    var videosElement = document.getElementById("video-container"); 
-
-    
-    function ajusterPosition() {
-        // Obtenez l'altitude du bas de l'élément par rapport à son parent
-        var topAltitude = icelandElement.offsetTop ;
-        var height = icelandElement.offsetHeight;
-
-
-        // Ajustez le style de l'élément "Iceland" en utilisant l'altitude du bas de la classe "image_container"
-        videosElement.style.top = topAltitude + 0.4*height + "px";
-
-
-    }
-
-    // Appelez la fonction initiale pour définir la position initiale
-    ajusterPosition();
-
-    // Ajoutez un écouteur d'événements de redimensionnement
-    window.addEventListener('resize', ajusterPosition);
-});
 
 document.addEventListener("DOMContentLoaded", function() {
 
-    var icelandElement = document.getElementById("Iceland");
+    var icelandElement = document.querySelector(".video-part-container");
     var bandElement = document.getElementById("first-black-band");
     var wallpaperElement = document.getElementById("wallpaper");  
 
