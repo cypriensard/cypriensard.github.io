6dcec453da8dd2ab1d2b515a0fb9a050b67a00df
cypriensard <155336260+cypriensard@users.noreply.github.com>
2024-01-25 15:14:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
     var imageContainer = document.querySelector(".image_container");
     var videoPartContainerElement = document.querySelector(".video-part-container");
-    var whitetransElement = document.getElementById("white-trans"); 
-
-    function ajusterPosition() {
-        // Obtenez l'altitude du bas de l'élément par rapport à son parent
-        var bottomAltitude = imageContainer.offsetTop + imageContainer.offsetHeight;
-
-        // Ajustez le style de l'élément "video-part-container" en utilisant l'altitude du bas de la classe "image_container"
-        videoPartContainerElement.style.top = bottomAltitude + "px";
-        whitetransElement.style.top = bottomAltitude + "px";
-    }
-
-    // Appelez la fonction initiale pour définir la position initiale
-    ajusterPosition();
-
-    // Ajoutez un écouteur d'événements de redimensionnement
-    window.addEventListener('resize', ajusterPosition);
-});
-
-
-document.addEventListener("DOMContentLoaded", function() {
-
+    var whitetransElement = document.getElementById("white-trans");
     var icelandElement = document.querySelector(".video-part-container");
     var bandElement = document.getElementById("first-black-band");
-    var wallpaperElement = document.getElementById("wallpaper");  
+    var wallpaperElement = document.getElementById("wallpaper");
 
-    
     function ajusterPosition() {
-        // Obtenez l'altitude du bas de l'élément par rapport à son parent
-        var topAltitude = icelandElement.offsetTop ;
+        var bottomAltitude = imageContainer.offsetTop + imageContainer.offsetHeight;
+        videoPartContainerElement.style.top = bottomAltitude + "px";
+        whitetransElement.style.top = bottomAltitude + "px";
+
+        var topAltitude = icelandElement.offsetTop;
         var hauteurband = icelandElement.offsetHeight;
         var hauteurwallpaper = wallpaperElement.offsetHeight;
-
-        // Ajustez le style de l'élément "Iceland" en utilisant l'altitude du bas de la classe "image_container"
         bandElement.style.height = topAltitude + hauteurband - hauteurwallpaper + "px";
-
-
     }
 
-    // Appelez la fonction initiale pour définir la position initiale
     ajusterPosition();
 
-    // Ajoutez un écouteur d'événements de redimensionnement
     window.addEventListener('resize', ajusterPosition);
 });
-
