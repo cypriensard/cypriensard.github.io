05776e584804c891515583d94a71e4b91aaa4962
cypriensard <155336260+cypriensard@users.noreply.github.com>
2024-02-18 13:41:12 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function scrollToImageContainer() {
+    // Faire défiler la page jusqu'au conteneur d'image
+    document.querySelector('.image_container').scrollIntoView({ behavior: 'smooth' });
+  }
